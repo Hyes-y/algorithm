20be6f68ad0ded6eb173006909c6dc42c8786bbc
Hyes-y <gptjs7970@naver.com>
2022-10-19 01:01:25 +0900

    221019 먼데이챌린지

---
+++ goorm/week03/04_순환하는수로(미해결).py
@@ -0,0 +1,84 @@
+n = int(input())
+graph = [[0] * (n+1) for _ in range(n+1)]
+
+for _ in range(n):
+	a, b = map(int, input().split())
+	graph[a][b] = 1
+	graph[b][a] = 1
+
+visited = [0] * (n+1)
+finished = [False] * (n+1)
+order = 0
+
+def dfs(node):
+	global order
+	
+	order += 1
+	visited[node] = order
+	
+	for idx, val in enumerate(graph[node]):
+		if val == 0:
+			continue
+			
+		if visited[idx] == 0:
+			graph[idx][node] = 0
+			if graph[idx] == [0] * (n+1):
+				continue
+			dfs(idx)
+		elif not finished[idx]:
+			break
+		else:
+			visited[order] = 0
+	finished[node] = True
+
+for i in range(1, n+1):
+	if graph[i] != [0] * (n+1):
+		dfs(i)
+		break
+
+print(finished)
+
+"""
+test case
+5
+5 2
+2 4
+4 3
+3 1
+1 2
+
+4
+1 2 3 4 
+--
+6
+1 3
+3 4
+4 5
+5 6
+6 2
+1 5
+
+4
+1 3 4 5 
+--
+5
+2 5
+5 3
+3 1
+1 4
+4 5
+
+4
+1 3 4 5
+--
+6
+1 2
+3 2
+3 4
+5 4
+5 6
+6 3
+
+4
+3 4 5 6
+"""
